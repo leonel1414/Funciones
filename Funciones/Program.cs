dbf73ea56aa054be19e3a888691a698ce9755400
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-03 21:09:52 -0300

    Modificaciones en Funciones

--- Funciones/Program.cs
+++ Funciones/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int CalcularCuadrado(int n) {
+    Console.WriteLine(n * n);
+    return n * n;
+}
+
+//CalcularCuadrado(2);
+
+void calcularAprobado(int a) {
+    if (a < 5)
+    {
+        Console.WriteLine("Reprobado");
+    }
+    else if (a == 5)
+    {
+        Console.WriteLine("Falta un punto para aprobar");
+    }
+    else {
+        Console.WriteLine("Haz aprobado");
+    }
+}
+
+calcularAprobado(1);
+calcularAprobado(5);
+calcularAprobado(9);
